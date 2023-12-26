b05f3623315f4bf0fd5911d79569e9b7ee300193
GhoulOwl <47019428+GhoulOwl@users.noreply.github.com>
2023-12-26 14:30:52 +0800

    upload

---
+++ js/about.6af4ce1f.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkindex"]=self["webpackChunkindex"]||[]).push([[443],{6589:function(n,e,u){u.r(e),u.d(e,{default:function(){return f}});var t=u(3396);const a={class:"about"},r=(0,t._)("h1",null,"This is an about page",-1),s=[r];function i(n,e){return(0,t.wg)(),(0,t.iD)("div",a,s)}var c=u(89);const o={},d=(0,c.Z)(o,[["render",i]]);var f=d}}]);
+//# sourceMappingURL=about.6af4ce1f.js.map
